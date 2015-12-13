63cd72e9296b9d01bd984213a6a34610bdd16157
Alexashooo <aleksandar.sunjic@gmail.com>
2015-12-13 21:12:16 +0100

    User sign in/out, works in a development environment, not in a production

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -44,6 +44,8 @@ Rails.application.configure do
   #is an example of default_url_options appropriate for a development environment
   #in config/environments/development.rb:
 
-  #config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {:address => "localhost", :port => 1025}
 
 end
